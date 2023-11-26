afe2a41ba404faf9ebc5c5f81bc5c08446733afd
K-yndon <131963244+K-yndon@users.noreply.github.com>
2023-11-26 19:30:16 +0000

    Add files via upload

---
+++ Coursework PY base.py
@@ -0,0 +1,14 @@
+
+"""
+Created on Tue Nov  7 19:53:39 2023
+
+@author: Kate
+"""
+
+import streamlit as st   
+import pandas as pd 
+
+header = st.container()
+
+with header:
+    st.title('Welcome to my project')
