4d5ed6dac464d0bc0026a70be66c304d434fa2f2
K-yndon <131963244+K-yndon@users.noreply.github.com>
2023-11-27 14:18:12 +0000

    Update Coursework_PY_base.py

--- Coursework_PY_base.py
+++ Coursework_PY_base.py
@@ -1,29 +1,24 @@
-
-"""
-Created on Tue Nov  7 19:53:39 2023
-
-@author: Kate
-"""
-
-import streamlit as st   
-import pandas as pd 
-
-header = st.container()
-inputs = st.container()
-with header:
-    st.title('Canoe Slalom Planner')
-
-with inputs:
-     username = st.text_input('Username','Enter username here')
-     password = st.text_input('Password','Enter password here')
-     st.markdown('Create account?')
-
+# hiding streamlit style
 hide_st_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
                 header {visibility: hidden;}
                 </style>
-                """
-                
+                """               
 st.markdown(hide_st_style, unsafe_allow_html=True)
+
+import streamlit as st   
+import pandas as pd 
+
+header = st.container()
+inputs = st.container()
+
+with header:
+    st.title('Canoe Slalom Planner')
+
+with inputs:
+     username = st.text_input('Username','Enter username here')
+     password = st.text_input('Password','Enter password here')
+     Create_clicked = st.button('Create account?')
+
