d337cb745fce56eee79cf161720521ac49b59163
K-yndon <131963244+K-yndon@users.noreply.github.com>
2023-12-04 13:37:51 +0000

    Update Coursework_PY_base.py

--- Coursework_PY_base.py
+++ Coursework_PY_base.py
@@ -19,8 +19,8 @@ names = ['John Smith','Rebecca Briggs']
 usernames = ['jsmith','rbriggs']
 passwords = ['123','456']
 
-
-authenticator = stauth.authenticate(names,usernames,passwords,
+hashed_passwords = stauth.Hasher(passwords).generate()
+authenticator = stauth.authenticate(names,usernames,hashed_passwords,
     'some_cookie_name','some_signature_key',cookie_expiry_days=30)
 name, authentication_status = authenticator.login('Login','main')
 
